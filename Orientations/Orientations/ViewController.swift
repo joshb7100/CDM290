1f9b9b81b6373d798dbe08f84d9610c59e4e6515
Josh <joshb7100@gmail.com>
2017-02-13 15:54:43 -0600

    Orientations

---
+++ Orientations/Orientations/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Orientations
+//
+//  Created by Josh T. Barrett on 2/13/17.
+//  Copyright © 2017 Josh Barrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var supportedInterfaceOrientations:UIInterfaceOrientationMask{
+        return UIInterfaceOrientationMask(rawValue: (UIInterfaceOrientationMask.portrait.rawValue | UIInterfaceOrientationMask.landscapeLeft.rawValue))
+    }
+
+}
+
