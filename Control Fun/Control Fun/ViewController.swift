3f6a0ec8de757643df0e3fe1210df9ed4d4ca52c
Josh <joshb7100@gmail.com>
2017-02-13 15:09:16 -0600

    Button Fun finished

--- Control Fun/Control Fun/ViewController.swift
+++ Control Fun/Control Fun/ViewController.swift
@@ -30,6 +30,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rightSwitch: UISwitch!
     
+    @IBOutlet weak var doSomethingButton: UIButton!
+    
     @IBAction func textFieldDoneEditing(sender: UITextField) {
         sender.resignFirstResponder()
     }
@@ -48,9 +50,40 @@ class ViewController: UIViewController {
         leftSwitch.setOn(setting, animated: true)
         rightSwitch.setOn(setting, animated: true)
     }
-
+    
     @IBAction func toggleControls(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0{
+            //"Switches" is selected
+            leftSwitch.isHidden = false
+            rightSwitch.isHidden = false
+            doSomethingButton.isHidden = true
+        }
+        else{
+            leftSwitch.isHidden = true
+            rightSwitch.isHidden = true
+            doSomethingButton.isHidden = false
+        }
     }
     
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let controller = UIAlertController(title: "Are You Sure?", message:nil, preferredStyle: .actionSheet)
+        let yesAction = UIAlertAction(title: "Yes, I'm Sure!", style: .destructive, handler: {action in
+            let msg = self.nameField.text!.isEmpty
+            ? "You can breathe easy, everything went OK."
+            : "You can breathe easy, \(self.nameField.text)," + "everything went OK"
+            let controller2 = UIAlertController(title: "Something Was Done", message: msg, preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title:"Phew!",style: .cancel, handler: nil)
+            controller2.addAction(cancelAction)
+            self.present(controller2, animated:true, completion: nil) })
+        let noAction = UIAlertAction(title:"No way!", style: .cancel, handler: nil)
+        controller.addAction(yesAction)
+        controller.addAction(noAction)
+        
+        if let ppc = controller.popoverPresentationController{
+            ppc.sourceView = sender
+            ppc.sourceRect = sender.bounds
+        }
+        present(controller, animated:true, completion: nil)
+    }
 }
 
