67819c16e1999a8c199e231b76f9760e118c2945
Josh <joshb7100@gmail.com>
2017-02-27 15:50:20 -0600

    Picker setup

---
+++ Pickers/Pickers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pickers
+//
+//  Created by Josh T. Barrett on 2/27/17.
+//  Copyright © 2017 Josh Barrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
