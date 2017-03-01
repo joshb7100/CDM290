9edf112d20a0ffa02de929963b18c226bf23ad47
Josh <joshb7100@gmail.com>
2017-02-28 22:28:51 -0600

    pickers 4/5

--- Pickers/Pickers/DatePickerViewController.swift
+++ Pickers/Pickers/DatePickerViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class DatePickerViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let date = NSDate()
+        datePicker.setDate(date as Date, animated:false)
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +35,14 @@ class DatePickerViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let date = datePicker.date
+        let message = "The date and time you selected is \(date)"
+        let alert = UIAlertController(title:"Date and Time Selected", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title:"That's so true!", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
 
 }
