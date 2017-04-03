829827083f56a1e54a8c3cd5416ed36424be9719
Josh <joshb7100@gmail.com>
2017-04-03 11:57:52 -0500

    variables

--- BillSplitter/BillSplitter/RestaurantViewController.swift
+++ BillSplitter/BillSplitter/RestaurantViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class RestaurantViewController: UIViewController {
+    var ftotal: Double
+    var numsplit: Int
+    var percent: [Double]
+    var finsplit: [Double]
+    var even: Int
+    var arrayNum: Int
+	var pretax: Double
+    var posttax: Double
+    var tip: Double
+    var paidby: String
+    var names: [String]
 
     override func viewDidLoad() {
         super.viewDidLoad()
