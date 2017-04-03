bb0566811e8cea89ea8d6be26899cbbb0473125c
Josh <joshb7100@gmail.com>
2017-04-03 16:23:46 -0500

    Restaurant View Half Done

--- BillSplitter/BillSplitter/RestaurantViewController.swift
+++ BillSplitter/BillSplitter/RestaurantViewController.swift
@@ -8,14 +8,30 @@
 
 import UIKit
 
-class RestaurantViewController: UIViewController {
+class RestaurantViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    private var tipValues:[Int] = []
+    private var peopleValues:[Int] = []
     //private var restaurantBill: restBill
+    @IBOutlet weak var pretaxField: UITextField!
+    @IBOutlet weak var posttaxField: UITextField!
+    @IBOutlet weak var tipPicker: UIPickerView!
+    @IBOutlet weak var peoplePicker: UIPickerView!
+    @IBOutlet weak var splitSwitch: UISegmentedControl!
+    @IBOutlet weak var splitTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         //restaurantBill = restBill()
+        splitTableView.isHidden = true
+        for i in 0...100{
+            tipValues.append(i)
+        }
+        for i in 1...30{
+            peopleValues.append(i)
+        }
+        tipPicker.selectRow(20, inComponent: 0, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,7 +39,53 @@ class RestaurantViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder()
+    }
 
+    
+    @IBAction func onTapGestureRecognized(_ sender: Any) {
+        pretaxField.resignFirstResponder()
+        posttaxField.resignFirstResponder()
+    }
+    
+    @IBAction func toggleSplit(_ sender: UISegmentedControl) {
+        if(sender.selectedSegmentIndex == 0){
+            splitTableView.isHidden = true
+        }
+        else{
+            splitTableView.isHidden = false
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if(pickerView == tipPicker){
+            return tipValues.count
+        }
+        else{
+            return peopleValues.count
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if(pickerView == tipPicker){
+            return String(tipValues[row]) + "%"
+        }
+        else{
+            return String(peopleValues[row])
+        }
+    }
+    
+    @IBAction func billOutput(_ sender: UIButton) {
+        performSegue(withIdentifier: "RestOutput", sender: nil)
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
---
+++ BillSplitter/BillSplitter/SplitTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SplitTableViewCell.swift
+//  BillSplitter
+//
+//  Created by Josh T. Barrett on 4/3/17.
+//  Copyright © 2017 Josh Barrett. All rights reserved.
+//
+
+import UIKit
+
+class SplitTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
